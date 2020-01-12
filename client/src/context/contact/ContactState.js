1f09f19769bacb685128d5fbad148f642bc35c55
Guran <robin.wassbjer@gmail.com>
2020-01-12 16:45:34 +0100

    Add contact filtering functionalities

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -30,7 +30,8 @@ const ContactState = props => {
         phone: '444-444-4444'
       }
     ],
-    current: null
+    current: null,
+    filtered: null
   };
 
   const [state, dispatch] = useReducer(contactReducer, initialState);
@@ -62,19 +63,28 @@ const ContactState = props => {
   };
 
   // Filter contacts
+  const filterContacts = text => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
   // Clear Filter
+  const clearFilter = () => {
+    dispatch({ type: CLEAR_FILTER });
+  };
 
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
         current: state.current,
+        filtered: state.filtered,
         addContact,
         deleteContact,
         setCurrent,
         clearCurrent,
-        updateContact
+        updateContact,
+        filterContacts,
+        clearFilter
       }}
     >
       {props.children}
